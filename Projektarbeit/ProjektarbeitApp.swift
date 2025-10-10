fa1b812a3fdff9f3f71dbd5f87390b9a2d96b0d2
rb807 <r.c.babcock@icloud.com>
2025-10-10 17:04:35 +0200

    Initial Commit

---
+++ Projektarbeit/ProjektarbeitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjektarbeitApp.swift
+//  Projektarbeit
+//
+//  Created by Ryan Babcock on 10.10.25.
+//
+
+import SwiftUI
+
+@main
+struct ProjektarbeitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjektarbeitTests/ProjektarbeitTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjektarbeitTests.swift
+//  ProjektarbeitTests
+//
+//  Created by Ryan Babcock on 10.10.25.
+//
+
+import Testing
+@testable import Projektarbeit
+
+struct ProjektarbeitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
