5d768b2b440c38149e2fb6abf59121e9f3294688
rb807 <r.c.babcock@icloud.com>
2025-12-15 09:17:32 +0100

    switched from async to delegate version

--- Projektarbeit/Location/LocationManager.swift
+++ Projektarbeit/Location/LocationManager.swift
@@ -9,7 +9,130 @@ import Foundation
 import CoreLocation
 import Combine
 
-/// Manages starting and stopping of location updates
+/// Manages starting and stopping of location updates with high accuracy
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    @Published var authorizationStatus: CLAuthorizationStatus?
+    @Published var samples: Int = 0
+    @Published var authorizationDenied: Bool = false
+    
+    private var fileHandler: FileHandle?
+    private var currentFileURL: URL?
+    private let locationManager = CLLocationManager()
+    private var isRecording: Bool = false
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        
+        // WICHTIG: Hier kannst du die Accuracy einstellen
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // Alternatives: kCLLocationAccuracyBestForNavigation für höchste Genauigkeit
+        
+        // Minimum distance (in meters) before location update
+        locationManager.distanceFilter = kCLDistanceFilterNone  // Alle Updates
+        
+        // Für Hintergrund-Updates (optional)
+        // locationManager.allowsBackgroundLocationUpdates = true
+        // locationManager.pausesLocationUpdatesAutomatically = false
+        
+        // Request authorization
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    /// Starts location updates with high accuracy
+    func startUpdates(path: URL) {
+        // Do nothing if already running
+        if isRecording {
+            NSLog("Location updates already running.")
+            return
+        }
+        
+        let filename = "location_data.csv"
+        let url = path.appendingPathComponent(filename)
+        currentFileURL = url
+        
+        // Create file and write csv table header
+        let header = "timestamp,latitude,longitude,horizontal_accuracy,vertical_accuracy,altitude,speed,course\n"
+        try? header.write(to: url, atomically: true, encoding: .utf8)
+        
+        // Open FileHandle
+        fileHandler = try? FileHandle(forWritingTo: url)
+        fileHandler?.seekToEndOfFile()
+        
+        samples = 0
+        isRecording = true
+        
+        // Start location updates
+        locationManager.startUpdatingLocation()
+        NSLog("Started location updates with accuracy: \(locationManager.desiredAccuracy)")
+    }
+    
+    /// Stops location updates
+    func stopUpdates() {
+        if !isRecording {
+            NSLog("Location updates not running.")
+            return
+        }
+        
+        isRecording = false
+        locationManager.stopUpdatingLocation()
+        
+        // Close FileHandle
+        try? fileHandler?.close()
+        fileHandler = nil
+        NSLog("Saved file to: \(currentFileURL?.path ?? "")")
+        NSLog("Stopped location updates.")
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard isRecording else { return }
+        
+        // Process all locations (usually just one, but can be multiple)
+        for location in locations {
+            samples += 1
+            
+            // Format: timestamp,latitude,longitude,horizontal_accuracy,vertical_accuracy,altitude,speed,course
+            let line = "\(location.timestamp.timeIntervalSince1970),\(location.coordinate.latitude),\(location.coordinate.longitude),\(location.horizontalAccuracy),\(location.verticalAccuracy),\(location.altitude),\(location.speed),\(location.course)\n"
+            
+            if let fh = fileHandler, let data = line.data(using: .utf8) {
+                fh.write(data)
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        NSLog("Location manager failed with error: \(error.localizedDescription)")
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+        
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            NSLog("Location authorization: Not Determined")
+        case .restricted:
+            NSLog("Location authorization: Restricted")
+            authorizationDenied = true
+        case .denied:
+            NSLog("Location authorization: Denied")
+            authorizationDenied = true
+        case .authorizedAlways:
+            NSLog("Location authorization: Always")
+        case .authorizedWhenInUse:
+            NSLog("Location authorization: When In Use")
+        @unknown default:
+            NSLog("Location authorization: Unknown")
+        }
+    }
+}
+
+
+// MARK: - Async Implementation (Commented Out)
+/*
+/// Old implementation using CLLocationUpdate.liveUpdates()
 class LocationManager: ObservableObject {
     
     @Published var authorizationStatus: CLAuthorizationStatus?
@@ -90,3 +213,4 @@ class LocationManager: ObservableObject {
         NSLog("Stopped live location updates.")
     }
 }
+*/
