4341eac83bfa44b3a40a544a8000d0aab63dfcc9
rb807 <r.c.babcock@icloud.com>
2025-12-17 11:41:17 +0100

    added managers as enviroment objects

--- Projektarbeit/ProjektarbeitApp.swift
+++ Projektarbeit/ProjektarbeitApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct ProjektarbeitApp: App {
+    // WICHTIG: Shared Managers für die ganze App
+    // Nur EINE Instanz, egal wie oft Views wechseln
+    @StateObject private var motionManager = MotionManager()
+    @StateObject private var cameraManager = CameraManager()
+    @StateObject private var locationManager = LocationManager()
+    @StateObject private var recordingTimer = RecordingTimer()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                // Teile Manager mit allen Child Views
+                .environmentObject(motionManager)
+                .environmentObject(cameraManager)
+                .environmentObject(locationManager)
+                .environmentObject(recordingTimer)
         }
     }
 }
